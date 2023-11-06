200eac77b20551baf84aa58b0c40dc990de608b2
Jackie Li <aurorali99@gmail.com>
2023-11-06 10:31:51 -0500

    Update, got some textured shadow shader + grass + special surface shader.

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -10,3 +10,48 @@ void GetNormal_float(float2 uv, out float3 Normal)
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
+
+static float2 SobelSamplePoints[9] = {
+    float2(-1, 1), float2(0, 1), float2(1, 1),
+    float2(-1, 0), float2(0, 0), float2(1, 0),
+    float2(-1, -1), float2(0, -1), float2(1, -1)
+};
+
+static float sobelXMatrix[9] = {
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+};
+
+static float sobelYMatrix[9] = {
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+};
+
+void DepthSobel_float(float2 UV, float Thickness, out float OUT) {
+    float2 sobel = 0;
+    [unroll] for (int i = 0; i < 9; i++) {
+        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + SobelSamplePoints[i] * Thickness);
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+    OUT = length(sobel);
+}
+
+
+void ColourSobel_float(float2 UV, float Thickness, out float OUT) {
+    float2 sobelR = 0;
+    float2 sobelG = 0;
+    float2 sobelB = 0;
+
+    [unroll] for (int i = 0; i < 9; i++) {
+        float3 rgb = SHADERGRAPH_SAMPLE_SCENE_COLOR(UV + SobelSamplePoints[i] * Thickness);
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+
+        sobelR += rgb.r * kernel;
+        sobelG += rgb.g * kernel;
+        sobelB += rgb.b * kernel;
+    }
+
+    OUT = max(length(sobelR), max(length(sobelG), length(sobelB)));
+}
