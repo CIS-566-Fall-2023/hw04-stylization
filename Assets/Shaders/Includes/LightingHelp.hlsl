3f0a3978658daa63daa177a2703fb0b9191a8d38
Caballo <1076318970@qq.com>
2023-11-08 13:53:19 +0800

    Done

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -31,7 +31,7 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
 #ifndef SHADERGRAPH_PREVIEW
 
     int pixelLightCount = GetAdditionalLightsCount();
-    
+
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPosition);
@@ -39,14 +39,14 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
         uint light_i = tmp[i % 4];
 
         half shadowAtten = light.shadowAttenuation * AdditionalLightRealtimeShadow(light_i, WorldPosition, light.direction);
-        
+
         half NdotL = saturate(dot(WorldNormal, light.direction));
         half distanceAtten = light.distanceAttenuation;
 
         half thisDiffuse = distanceAtten * shadowAtten * NdotL;
-        
+
         half rampedDiffuse = 0;
-        
+
         if (thisDiffuse < Thresholds.x)
         {
             rampedDiffuse = RampedDiffuseValues.x;
@@ -60,7 +60,13 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
             rampedDiffuse = RampedDiffuseValues.z;
         }
 
-        
+
+        if (shadowAtten * NdotL == 0)
+        {
+            rampedDiffuse = 0;
+
+        }
+
         if (light.distanceAttenuation <= 0)
         {
             rampedDiffuse = 0.0;
@@ -69,28 +75,61 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
         Color += max(rampedDiffuse, 0) * light.color.rgb;
         Diffuse += rampedDiffuse;
     }
-    
-    if (Diffuse <= 0.3)
-    {
-        Color = float3(0, 0, 0);
-        Diffuse = 0;
-    }
-    
 #endif
 }
 
-void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float2 Thresholds, out float3 OUT)
+float Random(float2 uv)
 {
-    if (Diffuse < Thresholds.x)
+    return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453);
+}
+
+void ChooseColor_float(float3 Highlight, float3 Shadow, float Diffuse, float Threshold, out float3 OUT)
+{
+    if (Diffuse < Threshold)
     {
         OUT = Shadow;
-    }
-    else if (Diffuse < Thresholds.y)
-    {
-        OUT = Midtone;
     }
     else
     {
         OUT = Highlight;
     }
 }
+
+void ChooseColor2_float(float3 WorldNormal, float3 WorldPosition, float3 CameraPosition, float3 BaseColor, float OutLineScale, out float3 OUT)
+{
+    float outline = dot(normalize(CameraPosition - WorldPosition), normalize(WorldNormal));
+    //if (outline < 0.45 && Random(Thresholds) < 0.8)
+    if (outline < OutLineScale)
+    {
+        //OUT = float3(0, 0, 0);
+        OUT = lerp(BaseColor, float3(0, 0, 0), 1 + OutLineScale - outline);
+    }
+    else
+    {
+        OUT = BaseColor;
+    }
+}
+
+float Gaussian(float x, float mu, float sigma)
+{
+    float a = (x - mu) / sigma;
+    return exp(-0.5 * a * a);
+}
+
+void NoiseColor_float(float3 EdgeColor, float3 CenterColor, float3 WorldNormal, float3 WorldPosition, float3 CameraPosition, float mu, float sigma, float NoiseScale, float NoiseIntensity, float3 NoiseTexture, out float3 OUT)
+{
+    float distanceFromCenter = dot(normalize(CameraPosition - WorldPosition), normalize(WorldNormal));
+
+    float noiseValue = (NoiseTexture.r + NoiseTexture.g + NoiseTexture.b) / 3.0;
+
+    float gaussianValue = Gaussian(distanceFromCenter, mu, sigma);
+
+    gaussianValue = saturate(gaussianValue * (1.0 + NoiseIntensity * (noiseValue - 0.5)));
+
+    float3 color = lerp(CenterColor, EdgeColor, gaussianValue);
+
+    OUT = lerp(color, float3(0, 0, 0), gaussianValue + NoiseScale);
+}
+
+
+
