5d019d71da12fad2cd1ea96af853746490665ee2
Rachel Lin <raclin555@gmail.com>
2023-11-07 20:29:32 -0500

    outlines look weird

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -1,4 +1,28 @@
+#ifndef SOBELOUTLINES_INCLUDED
+#define SOBELOUTLINES_INCLUDED
+
 SAMPLER(sampler_point_clamp);
+
+// pts ssampled relative to starting point
+static float2 sobelSamplePoints[9] = {
+    float2(-1, 1), float2(0, 1), float2(1, 1), 
+    float2(-1, 0), float2(0, 0), float2(1, 1),
+    float2(-1, -1), float2(0, -1), float2(1, -1)
+};
+
+// wts for x-component
+static float sobelXMatrix[9] = {
+    1, 0, -1, 
+    2, 0, -2, 
+    1, 0, -1
+};
+
+//wts for y-component
+static float sobelYMatrix[9] = {
+    1, 2, 1, 
+    0, 0, 0, 
+    -1, -2, -1
+};
 
 void GetDepth_float(float2 uv, out float Depth)
 {
@@ -6,11 +30,20 @@ void GetDepth_float(float2 uv, out float Depth)
 }
 
 
-void GetNormal_float(float2 uv, out float3 Normal)
+void GetNormal_float(float2 uv, out float3 Normal) 
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
 
-void Sobel_Find_Depth() {
+void DepthSobel_float(float2 UV, float Thickness, out float Out) {
+    float2 sobel = 0;
 
+    [unroll] for (int i = 0; i < 9; i++) {
+        // float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelSamplePoints[i] * Thickness);
+        float depth = 
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+    Out = length(sobel);
 }
+
+#endif
