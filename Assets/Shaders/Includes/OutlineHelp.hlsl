9229af56f0b61a5d32dc4eb413f2b2b131a9efdc
Thomas Shaw <printer.83mph@gmail.com>
2023-11-07 23:54:38 -0500

    Add depth based outlines

--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -10,3 +10,36 @@ void GetNormal_float(float2 uv, out float3 Normal)
 {
     Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
 }
+
+static float2 sobelOffsets[9] = {
+    float2(-1, 1), float2(0, 1), float2(1, 1),
+    float2(-1, 0), float2(0, 0), float2(1, 0),
+    float2(-1, -1), float2(0, -1), float2(1, -1)
+};
+
+static float sobelXMatrix[9] = {
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+};
+
+static float sobelYMatrix[9] = {
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+};
+
+void GetDepthSobel_float(float2 UV, float Thickness, out float Out)
+{
+    float2 sobel = 0;
+
+    float2 offsetFactor = float2(1.f / _ScreenParams.x, 1.f / _ScreenParams.y) * Thickness;
+    
+    [unroll] for (int i = 0; i < 9; ++i)
+    {
+        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelOffsets[i] * offsetFactor);
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+
+    Out = length(sobel);
+}
