7ce5f19ecf9a83667c786c299ad2a075811d454a
dinethmeegoda <35506196+dinethmeegoda@users.noreply.github.com>
2023-11-23 04:15:58 -0500

    almost done

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -59,7 +59,13 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
         {
             rampedDiffuse = RampedDiffuseValues.z;
         }
+        
+        
+        if (shadowAtten * NdotL == 0)
+        {
+            rampedDiffuse = 0;
 
+        }
         
         if (light.distanceAttenuation <= 0)
         {
@@ -69,13 +75,6 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
         Color += max(rampedDiffuse, 0) * light.color.rgb;
         Diffuse += rampedDiffuse;
     }
-    
-    if (Diffuse <= 0.3)
-    {
-        Color = float3(0, 0, 0);
-        Diffuse = 0;
-    }
-    
 #endif
 }
 
@@ -92,5 +91,136 @@ void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Di
     else
     {
         OUT = Highlight;
+    }
+}
+
+void ChooseColor3Tone_float(float3 Highlight, float3 Shadow, float3 MidTone, float Diffuse, float Threshold, float Threshold2, out float3 OUT, out float3 SHADOW_HATCH_MASK, out float3 MIDTONE_HATCH_MASK)
+{
+    SHADOW_HATCH_MASK = float3(0.0f, 0.0f, 0.0f);
+    MIDTONE_HATCH_MASK = float3(0.0f, 0.0f, 0.0f);
+    if (Diffuse < Threshold && Diffuse < Threshold2)
+    {
+        OUT = Shadow;
+        SHADOW_HATCH_MASK = float3(1.0f, 1.0f, 1.0f);
+    }
+    else if (Diffuse < Threshold2)
+    {
+        OUT = MidTone;
+        MIDTONE_HATCH_MASK = float3(1.0f, 1.0f, 1.0f);
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
+
+void ChooseThreeToneSimple_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float Threshold1, float Threshold2, out
+float3 OUT)
+{
+    if (Diffuse < Threshold1)
+    {
+        OUT = Shadow;
+    }
+    else if (Diffuse < Threshold2)
+    {
+        OUT = Midtone;
+    }
+    else
+    {
+        OUT = Highlight;
+    }
+}
+
+void MapCircle_float(float3 innerColor, float3 outerColor, float3 backgroundColor, float colorOffset, float xOffset, float yOffset, float radius, float falloff, float falloffLimit, float xValue, float yValue, out float3 OUT)
+{
+    float circleValue = pow((xValue - xOffset), 2) + pow((yValue - yOffset), 2);
+    //float3 realOuterColor = lerp(outerColor, backgroundColor, 0.5);
+    if (circleValue <= pow(radius, 2))
+    {
+        float circleP = sqrt(circleValue) / radius;
+        OUT = lerp(innerColor, outerColor, circleP * colorOffset);
+
+    }
+    else
+    {
+        // Not a circle
+        float outerP = (sqrt(circleValue) - radius) / (falloffLimit - radius);
+        OUT = lerp(outerColor, backgroundColor, outerP * falloff);
+        //OUT = float3(defaultC, defaultC, defaultC);
+    }
+}
+
+
+float GetBias(float time, float bias)
+{
+    return (time / ((((1.0 / bias) - 2.0) * (1.0 - time)) + 1.0));
+}
+
+float GetGain(float time, float gain)
+{
+  if(time < 0.5)
+    return GetBias(time * 2.0,gain)/2.0;
+  else
+    return GetBias(time * 2.0 - 1.0,1.0 - gain)/2.0 + 0.5;
+}
+
+void ComputeCloudColor_float(float3 leftRightColor, float3 DColor1, float3 DColor2, float3 DColor3, float3 DColor4, 
+float3 sunColor, float3 normal, float threshold, float Dthreshold1, float Dthreshold2, float Dthreshold3, float yPosition, float zPosition, float bias, out float3 OUT)
+{
+    float biasedY = GetGain(yPosition, bias);
+    float3 backFrontColor;
+    if (zPosition <= Dthreshold1)
+    {
+        backFrontColor = DColor1;
+    }
+    else if (zPosition <= Dthreshold2)
+    {
+        backFrontColor = DColor2;
+    }
+    else if (zPosition <= Dthreshold3)
+    {
+        backFrontColor = DColor3;
+    }
+    else
+    {
+        backFrontColor = DColor4;
+    }
+    float3 gradientColor = lerp(leftRightColor, backFrontColor, biasedY);;
+    if (dot(normal, float3(1, 0, 0)) <= threshold) 
+    {
+        OUT = sunColor;
+    }
+    else
+    {
+        OUT = gradientColor;
+    }
+    
+}
+
+void computeFoamColor_float(float dotProd, float foamThreshold, float zDistance, float foamBias, out float OUT)
+{
+    if (dotProd < foamThreshold)
+    {
+        OUT = 1.f * GetBias((1 - zDistance), foamBias);
+    }
+    else
+    {
+        OUT = 0.0;
+    }
+}
+
+void computePalmColor_float(float3 treeColor, float3 highlightColor, float3 shadowColor, float xDistance, float yDistance, float threshold, float threshold2, out float3 OUT)
+{
+    if ((1 - xDistance) < threshold2)
+    {
+        OUT = highlightColor;
+    }
+    else if (yDistance * threshold < xDistance)
+    {
+        OUT = treeColor;
+    }
+    else
+    {
+        OUT = shadowColor;
     }
 }
--- Assets/Shaders/Includes/OutlineHelp.hlsl
+++ Assets/Shaders/Includes/OutlineHelp.hlsl
@@ -8,5 +8,69 @@ void GetDepth_float(float2 uv, out float Depth)
 
 void GetNormal_float(float2 uv, out float3 Normal)
 {
-    Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
+    //Normal = SAMPLE_TEXTURE2D(_NormalsBuffer, sampler_point_clamp, uv).rgb;
+}
+
+// SOBEL
+static float2 sobelSamplePoints[9] =
+{
+    float2(-1, 1), float2(0, 1), float2(1, 1),
+    float2(-1, 0), float2(0, 0), float2(1, 1),
+    float2(-1, -1), float2(0, -1), float2(1, -1),
+};
+
+// Horizontal Weights
+static float sobelXMatrix[9] =
+{
+    1, 0, -1,
+    2, 0, -2,
+    1, 0, -1
+};
+
+// Vertical Weights
+static float sobelYMatrix[9] =
+{
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1
+};
+
+void DepthSobel_float(float2 UV, float Thickness, float2 screenRatio, out
+
+float OUT)
+{
+    float2 sobel = 0;
+    
+    [unroll] for (int i = 0; i < 9; i++)
+    {
+        float depth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV + sobelSamplePoints[i] * Thickness * screenRatio);
+        sobel += depth * float2(sobelXMatrix[i], sobelYMatrix[i]);
+    }
+
+    OUT = length(sobel);
+}
+
+void ReturnMainTex_float(float2 UV, out float3 OUT)
+{
+    OUT = SAMPLE_TEXTURE2D(_MainTex, sampler_point_clamp, UV).rgb;
+}
+
+void ColorSobel_float(float2 UV, float Thickness, float2 screenRatio, out float OUT)
+{
+    float2 sobelR = 0;
+    float2 sobelG = 0;
+    float2 sobelB = 0;
+    
+    //[unroll]
+    for (int i = 0; i < 9; i++)
+    {
+        float3 rgb = SAMPLE_TEXTURE2D(_MainTex, sampler_point_clamp, UV + sobelSamplePoints[i] * Thickness * screenRatio).rgb;
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+        
+        sobelR += rgb.r * kernel;
+        sobelG += rgb.g * kernel;
+        sobelB += rgb.b * kernel;
+    }
+    
+    OUT = max(length(sobelR), max(length(sobelG), length(sobelB)));
 }
