479a5045c815059f4f7623288eff01503ea458b2
leoteemo <leowangfcb@gmail.com>
2023-12-16 05:41:02 -0500

    almost

---
+++ Assets/Scripts/PartyScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PartyScript : MonoBehaviour
+{
+    public Material[] materials;
+    private MeshRenderer meshRenderer;
+    int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            index = (index + 1) % materials.Count();
+            SwapToNextMaterial(index);
+        }
+    }
+
+    void SwapToNextMaterial(int index)
+    {
+        meshRenderer.material = materials[index % materials.Count()];
+    }
+}
