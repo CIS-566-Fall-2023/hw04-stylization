cd904f1f9bedd738ad2a02ec85a33c9709e86b26
Cindy Xu <CINDYXU.CHAPIN@GMAIL.COM>
2023-12-15 19:00:54 -0500

    submission

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -21,8 +21,10 @@ void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction,
 #endif
 }
 
+float3 ShadowEdgeColor;
+
 void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
-    float2 Thresholds, float3 RampedDiffuseValues,
+    float Threshold, float Threshold2, float3 RampedDiffuseValues,
     out float3 Color, out float Diffuse)
 {
     Color = float3(0, 0, 0);
@@ -31,7 +33,7 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
 #ifndef SHADERGRAPH_PREVIEW
 
     int pixelLightCount = GetAdditionalLightsCount();
-    
+
     for (int i = 0; i < pixelLightCount; ++i)
     {
         Light light = GetAdditionalLight(i, WorldPosition);
@@ -39,19 +41,19 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
         uint light_i = tmp[i % 4];
 
         half shadowAtten = light.shadowAttenuation * AdditionalLightRealtimeShadow(light_i, WorldPosition, light.direction);
-        
+
         half NdotL = saturate(dot(WorldNormal, light.direction));
         half distanceAtten = light.distanceAttenuation;
 
         half thisDiffuse = distanceAtten * shadowAtten * NdotL;
-        
+
         half rampedDiffuse = 0;
-        
-        if (thisDiffuse < Thresholds.x)
+
+        if (thisDiffuse < Threshold)
         {
             rampedDiffuse = RampedDiffuseValues.x;
         }
-        else if (thisDiffuse < Thresholds.y)
+        else if (thisDiffuse < Threshold2)
         {
             rampedDiffuse = RampedDiffuseValues.y;
         }
@@ -60,37 +62,68 @@ void ComputeAdditionalLighting_float(float3 WorldPosition, float3 WorldNormal,
             rampedDiffuse = RampedDiffuseValues.z;
         }
 
-        
+
+        if (shadowAtten * NdotL == 0)
+        {
+            rampedDiffuse = 0;
+
+        }
+
         if (light.distanceAttenuation <= 0)
         {
             rampedDiffuse = 0.0;
         }
 
+
         Color += max(rampedDiffuse, 0) * light.color.rgb;
         Diffuse += rampedDiffuse;
     }
-    
-    if (Diffuse <= 0.3)
-    {
-        Color = float3(0, 0, 0);
-        Diffuse = 0;
-    }
-    
 #endif
 }
 
-void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float2 Thresholds, out float3 OUT)
+void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float Threshold, float Threshold2, float3 ShadowEdgeColor, out float3 OUT)
 {
-    if (Diffuse < Thresholds.x)
+    if (Diffuse < Threshold - .01)
     {
         OUT = Shadow;
     }
-    else if (Diffuse < Thresholds.y)
+    else if (Diffuse < Threshold + 0.01)
+    {
+        OUT = ShadowEdgeColor;
+    }
+    else if (Diffuse < Threshold2)
     {
         OUT = Midtone;
     }
-    else
+    else 
     {
         OUT = Highlight;
     }
+
 }
+
+// perlin noise function referenced from ShaderToy
+float2 hash(float2 p)
+{
+    p = float2(dot(p, float2(127.1, 311.7)), dot(p, float2(269.5, 183.3)));
+    return -1.0 + 2.0 * frac(sin(p) * 43758.5453123);
+}
+
+float noise(float2 p)
+{
+    const float K1 = 0.366025404; // (sqrt(3)-1)/2;
+    const float K2 = 0.211324865; // (3-sqrt(3))/6;
+
+    float2 i = floor(p + (p.x + p.y) * K1);
+    float2 a = p - i + (i.x + i.y) * K2;
+    float m = step(a.y, a.x);
+    float2 o = float2(m, 1.0 - m);
+    float2 b = a - o + K2;
+    float2 c = a - 1.0 + 2.0 * K2;
+    float3 h = max(0.5 - float3(dot(a, a), dot(b, b), dot(c, c)), 0.0);
+    float3 n = h * h * h * h * float3(dot(a, hash(i + 0.0)), dot(b, hash(i + o)), dot(c, hash(i + 1.0)));
+    return dot(n, float3(70.0, 70.0, 70.0));
+}
+
+// Add noise to ShadowEdgeColor
+
